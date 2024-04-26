7797f132700f4e97e68c3198c582184923ff62a8
Ariana <arianafamiliar@hotmail.com>
2024-04-26 17:36:03 -0400

    Revert "first commit"
    This reverts commit d871767060c443f47dbc9ec7ed9c2086442b3f57.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-# run feature extraction (CaPTk version 1.8.1)
-
-FROM cbica/captk:2021.03.29 
+# run pre-processing (CaPTk version 1.8.1)
+#   outputs:
+#       [subID]_T1_to_SRI.nii.gz
+#       [subID]_T1CE_to_SRI.nii.gz
+#       [subID]_T2_to_SRI.nii.gz
+#       [subID]_FL_to_SRI.nii.gz
+FROM cbica/captk:2021.03.29 AS preproc 
 
 COPY run.py run.py
-COPY radiomic_feature_params_20230725.csv radiomic_feature_params_20230725.csv
-ENTRYPOINT [ "python3", "run.py" ]
+CMD [ "python3", "run.py" ]
--- run.py
+++ run.py
@@ -1,12 +1,3 @@
-
-# mapping for tumor seg:
-# | Image type      | imageID |
-# | ----------- | ----------- |
-# | T2w-FLAIR      | 000       |
-# | T1w   | 001        |
-# | T1w post-contrast   | 002        |
-# | T2w   | 003        |
-
 import os
 import shutil
 from glob import glob
@@ -16,48 +7,55 @@ CaPTk_dir='/opt/captk/1.8.1/usr' # path to install (in Docker container)
 unique_subs=[]
 for im_file in glob("input/*.nii*"):
     im_file_name = im_file.split('/')[-1]
-    if '_0000_ss_norm' in im_file_name:
-        this_sub = im_file_name.split('_0000_ss_norm')[0]
+    if '_0000.nii' in im_file_name:
+        this_sub = im_file_name.split('_0000.nii')[0]
         if this_sub not in unique_subs:
             unique_subs.append(this_sub)
 
+<<<<<<< HEAD
 print(f'{len(unique_subs)} subjects found for processing.')
 
 print(f'{len(unique_subs)} subjects found for processing.')
 
+=======
+>>>>>>> parent of d871767 (first commit)
 # run pre-processing on each unique subID in the input dir
 # then setup pre-processed files for input into nnUNet tumor seg model
 for sub in unique_subs:
+    # make output directory for this subject
+    output_dir=f'{sub}'
+    os.makedirs(output_dir) 
     # run the main pipeline
-    fl_im = glob(f'input/{sub}_0000_ss_norm*')[0]
-    t1_im = glob(f'input/{sub}_0001_ss_norm*')[0]
-    t1ce_im = glob(f'input/{sub}_0002_ss_norm*')[0]
-    t2_im = glob(f'input/{sub}_0003_ss_norm*')[0]
-    seg_file = glob(f'input/{sub}_pred_brainTumorSeg*')[0]
-
+    fl_im = glob(f'input/{sub}_0000.nii*')[0]
+    t1_im = glob(f'input/{sub}_0001.nii*')[0]
+    t1ce_im = glob(f'input/{sub}_0002.nii*')[0]
+    t2_im = glob(f'input/{sub}_0003.nii*')[0]
     print(f" ========== Running CaPTk BraTSPipeline for subject ID {sub} ==========")
+    os.system(f'{CaPTk_dir}/bin/BraTSPipeline \
+                -t1 {t1_im} \
+                -t1c {t1ce_im} \
+                -t2 {t2_im} \
+                -fl {fl_im} \
+                --skullStrip 0 \
+                --brainTumor 0 \
+                --interFiles 0 \
+                -o {sub}')
+    # move result files to final output directory
+    preproc_output_dir = 'preprocessed'
+    if not os.path.exists(preproc_output_dir):
+        os.makedirs(preproc_output_dir)
+    shutil.move(f'{sub}/T1_to_SRI.nii.gz',f'{preproc_output_dir}/{sub}_T1_to_SRI.nii.gz')
+    shutil.move(f'{sub}/T1CE_to_SRI.nii.gz',f'{preproc_output_dir}/{sub}_T1CE_to_SRI.nii.gz')
+    shutil.move(f'{sub}/T2_to_SRI.nii.gz',f'{preproc_output_dir}/{sub}_T2_to_SRI.nii.gz')
+    shutil.move(f'{sub}/FL_to_SRI.nii.gz',f'{preproc_output_dir}/{sub}_FL_to_SRI.nii.gz')
 
-    # binarize the tumor segmentation ROI
-    bin_roi_fn = f'{sub}_pred_brainTumorSeg.nii.gz'
-    os.system(f'{CaPTk_dir}/bin/Utilities -i {seg_file} -o {bin_roi_fn} -cm')
+    # cleanup
+    shutil.rmtree(f'{sub}')
 
-    # run feature extraction
-    feat_file = 'radiomic_feature_params_20230725.csv'
-    os.system(f'{CaPTk_dir}/bin/FeatureExtraction \
-        -n {sub} \
-        -i {t1_im},{t1ce_im},{t2_im},{fl_im} \
-        -t T1,T1Gd,T2,FLAIR \
-        -m {bin_roi_fn} \
-        -r 1 \
-        -l "tumor_core" \
-        -p {feat_file} \
-        -o "output/"')
-
-# merge all of the individual subject files into one output CSV
-one_csv = glob(f'output/results_*.csv')[0]
-os.system(f'cat {one_csv} | head -n1 > output/radiomic_features.csv')
-os.system('for f in output/results_*.csv; do cat "`pwd`/$f" | tail -n +2 >> output/radiomic_features.csv; done ')
-
-# delete the subject CSVs
-for subj_csv in glob('output/results_*.csv'):
-    os.remove(subj_csv)
+# mapping for tumor seg:
+# | Image type      | imageID |
+# | ----------- | ----------- |
+# | T2w-FLAIR      | 000       |
+# | T1w   | 001        |
+# | T1w post-contrast   | 002        |
+# | T2w   | 003        |
